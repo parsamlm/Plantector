f43506ce989b2b062cfe3c2a3af8f697b37a8971
Parsa Moslem <p.moslem1378@gmail.com>
2021-09-16 23:51:49 +0430

    UI 90 completed

--- app/src/main/java/com/example/plantector/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/plantector/ui/settings/SettingsActivity.kt
@@ -2,11 +2,17 @@ package com.example.plantector.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.plantector.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        findViewById<ImageView>(R.id.back_btn_settings).setOnClickListener{
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/plantector/ui/store/StoreFragment.kt
+++ app/src/main/java/com/example/plantector/ui/store/StoreFragment.kt
@@ -1,12 +1,15 @@
 package com.example.plantector.ui.store
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.plantector.R
+import com.example.plantector.ui.settings.SettingsActivity
 
 class StoreFragment : Fragment() {
 
@@ -21,6 +24,13 @@ class StoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_store, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ImageView>(R.id.settings_home_iv).setOnClickListener {
+            startActivity(Intent(activity, SettingsActivity()::class.java))
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
